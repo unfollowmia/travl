70580627823384cfcd9591b8f6d897bf7ab67507
unfollowmia <unfollowmia@users.noreply.github.com>
2026-07-18 23:17:27 +0000

    chore: initial commit

--- src/theme/index.ts
+++
@@ -1,7 +0,0 @@
-export { Colors } from './colors';
-export type { ColorKey } from './colors';
-
-export { Typography } from './typography';
-export type { TypographyKey } from './typography';
-
-export { Spacing, BorderRadius, Shadow } from './spacing';
